70bde6076566245b065b25aa474cd3cd0745f381
ho04029 <ho04029@naver.com>
2022-11-02 20:19:31 +0900

    title변경

--- src/pages/Diary.js
+++ src/pages/Diary.js
@@ -14,6 +14,11 @@ const Diary = () => {
   const [data, setData] = useState();
 
   useEffect(() => {
+    const titleElement = document.getElementsByTagName("title")[0];
+    titleElement.innerHTML = `감정일기장 ${id}번째 일기`;
+  }, [id]);
+
+  useEffect(() => {
     if (diaryList.length > 1) {
       const targetDiary = diaryList.find(
         (item) => parseInt(item.id) === parseInt(id)
